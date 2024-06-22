83d7dab29fce4bdd27bf059774c21419c657da7c
Nick Schneble <nschneble@users.noreply.github.com>
2024-06-21 23:32:04 -0400

    add concern for easy lookups

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,6 @@
 # Base class for data models
 class ApplicationRecord < ActiveRecord::Base
+  include Findable
+
   primary_abstract_class
 end
---
+++ app/models/concerns/findable.rb
@@ -0,0 +1,14 @@
+# This concern provides convenience methods for quickly finding records
+module Findable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :disabled, -> { where(disabled: true) }
+  end
+
+  class_methods do
+    def s(name)
+      find_by(name:)
+    end
+  end
+end
