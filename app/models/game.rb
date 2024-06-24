7e0236a068cbbc1bd83d5c014e2f1694ecc935b3
Nick Schneble <nschneble@users.noreply.github.com>
2024-06-23 21:17:53 -0400

    add helpers for # players and game length

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,12 @@ class Game < ApplicationRecord
   belongs_to :designer
   has_and_belongs_to_many :collections
   has_many :players, through: :collections
+
+  def number_of_players
+    "#{play_count.begin.to_i} - #{play_count.end.to_i}"
+  end
+
+  def game_length_in_minutes
+    "#{play_time.begin.to_i} - #{play_time.end.to_i} #{I18n.t("games.min")}"
+  end
 end
