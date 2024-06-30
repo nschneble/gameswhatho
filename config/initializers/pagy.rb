7c7120dda37a996ec16108712780b587eee796f0
Nick Schneble <nschneble@users.noreply.github.com>
2024-06-30 15:51:44 -0400

    override Pagy en locale

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -204,6 +204,7 @@
 #                   filepath: 'path/to/pagy-xyz.yml',
 #                   pluralize: lambda{ |count| ... } )
 
+Pagy::I18n.load(locale: 'en', filepath: 'config/locales/en.pagy.yml')
 
 # I18n extra: uses the standard i18n gem which is ~18x slower using ~10x more memory
 # than the default pagy internal i18n (see above)
