9e020da5a83235e4aabee8d19c34756a232a3dad
Nick Schneble <nschneble@users.noreply.github.com>
2024-06-30 12:36:43 -0400

    add Gamewright games

---
+++ db/seeds/designers/gamewright.rb
@@ -0,0 +1,24 @@
+# Gamewright
+gamewright = Designer.create_with(
+  website: "https://gamewright.com"
+).find_or_create_by!(
+  name: "Gamewright"
+)
+
+# Sushi Go!
+Game.create_with(
+  play_count: 2..5,
+  play_time: ..15,
+  designer: gamewright
+).find_or_create_by!(
+  name: "Sushi Go!"
+)
+
+# Abandon All Artichokes
+Game.create_with(
+  play_count: 2..4,
+  play_time: ..20,
+  designer: gamewright
+).find_or_create_by!(
+  name: "Abandon All Artichokes"
+)
--- db/seeds/players/nick.rb
+++ db/seeds/players/nick.rb
@@ -2,4 +2,7 @@
 Player.find_or_create_by!(name: "Nick")
 
 # seed Nick's collection of board games
-Player.s("Nick").collections.first.games << Game.s("Happy Little Dinosaurs")
+default_collection = Player.s("Nick").collections.first
+default_collection.games << Game.s("Happy Little Dinosaurs")
+default_collection.games << Game.s("Sushi Go!")
+default_collection.games << Game.s("Abandon All Artichokes")
