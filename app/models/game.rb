10fec96806eee070650186875617a5c9a47a5d35
Nick Schneble <nschneble@users.noreply.github.com>
2024-06-30 16:55:17 -0400

    set badge color based on game length

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,11 +6,28 @@ class Game < ApplicationRecord
 
   include RangeUtils
 
+  GAME_LENGTH_IN_MIN = {
+    short: 30,
+    medium: 60
+  }
+
   def number_of_players
     format_range(play_count)
   end
 
   def game_length_in_minutes
     "#{format_range(play_time)} #{I18n.t('games.min')}"
+  end
+
+  def short?
+    play_time.end.to_i <= GAME_LENGTH_IN_MIN[:short]
+  end
+
+  def medium?
+    play_time.end.to_i <= GAME_LENGTH_IN_MIN[:medium]
+  end
+
+  def long?
+    play_time.end.to_i > GAME_LENGTH_IN_MIN[:medium]
   end
 end
