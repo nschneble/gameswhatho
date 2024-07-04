0725799028de68a90b9945d2d9aa5629bc8ed455
Nick Schneble <nschneble@users.noreply.github.com>
2024-07-04 16:21:43 -0400

    let's go TURBO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 class ApplicationController < ActionController::Base
   include Localizable
   include Pagy::Backend
+
+  before_action :turbo_frame_request_variant
+
+  private
+
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
+  end
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,3 +1,12 @@
 # Helper methods for board games
 module GamesHelper
+  def play_time_badge_colors(game)
+    if game.short?
+      "text-emerald-500 bg-emerald-100/60"
+    elsif game.medium?
+      "text-yellow-500 bg-yellow-100/60"
+    else
+      "text-red-500 bg-red-100/60"
+    end
+  end
 end
