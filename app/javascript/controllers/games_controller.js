b27214904519a6c4ffd5ddc62b66820205dbed60
Nick Schneble <nschneble@users.noreply.github.com>
2024-07-05 12:42:01 -0400

    refactor modal close logic

--- app/javascript/controllers/games_controller.js
+++ app/javascript/controllers/games_controller.js
@@ -2,44 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="games"
 export default class extends Controller {
-  connect() {
-    document.addEventListener("click", function(event) {
-      if (event.target.matches(".button-close-modal") || !event.target.closest(".modal")) {
-        const modal = document.querySelector("dialog");
-        modal.close();
-      }
-    });
-  }
-
-  // Fetches the JSON data for a board game
-  show(event) {
-    const id = this.data.get("id");
-
+  show() {
+    const id = this.data.get("id")
     fetch(`/games/${id}.json`)
       .then((response) => {
         if (!response.ok) {
-          const status = response.status;
-          const httpStatusUrl = `https://httpstatuses.io/${status}`;
-
           throw new Error(`Couldn't retrieve board game details. ` +
-            `The server returned an HTTP status of <a href="${httpStatusUrl}" target="_blank">${status}</a>`);
+            `The server returned a ${response.status} HTTP status.`)
         }
-        return response.json();
+        return response.json()
       })
       .then((data) => {
-        const modal = document.querySelector("dialog");
-        const game = data.game;
+        const modal = document.querySelector("dialog")
+        const game = data.game
 
         modal.querySelector("[data-name]").textContent = game.name
         modal.querySelector("[data-players]").textContent = game.number_of_players
         modal.querySelector("[data-length]").textContent = game.game_length_in_minutes
         modal.querySelector("[data-designer]").textContent = game.designer_name
-        modal.querySelector("[data-designer]").setAttribute("href", game.designer_website);
+        modal.querySelector("[data-designer]").setAttribute("href", game.designer_website)
+        modal.showModal()
 
-        modal.showModal();
+        document.addEventListener("click", closeModal)
       })
       .catch((error) => {
-        alert(error);
-      });
+        alert(error)
+      })
+  }
+}
+
+function closeModal(event) {
+  if (event.target.matches(".button-close-modal") || !event.target.closest(".modal")) {
+    const modal = document.querySelector("dialog")
+    modal.close()
+    document.removeEventListener("click", closeModal)
   }
 }
