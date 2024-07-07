42303bdf1b042c3d23212d8b62de1606b18d2e62
Nick Schneble <nschneble@users.noreply.github.com>
2024-07-06 20:52:32 -0400

    seed Dolphin Hat Games

---
+++ db/seeds/designers/dolphin_hat_games.rb
@@ -0,0 +1,15 @@
+# Dolphin Hat Games
+Designer.create_with(
+  website: "https://dolphinhat.com"
+).find_or_create_by!(
+  name: "Dolphin Hat Games"
+)
+
+# Taco Cat Goat Cheese Pizza
+Game.create_with(
+  play_count: 3..8,
+  play_time: 10..,
+  designer: Designer.s("Dolphin Hat Games")
+).find_or_create_by!(
+  name: "Taco Cat Goat Cheese Pizza"
+)
--- db/seeds/players/nick.rb
+++ db/seeds/players/nick.rb
@@ -3,8 +3,9 @@ Player.find_or_create_by!(name: "Nick")
 
 # seed Nick's collection of board games
 default_collection = Player.s("Nick").collections.first
-default_collection.games << Game.s("Happy Little Dinosaurs")
-default_collection.games << Game.s("Sushi Go!")
 default_collection.games << Game.s("Abandon All Artichokes")
 default_collection.games << Game.s("Everdell")
 default_collection.games << Game.s("Everdell Spirecrest")
+default_collection.games << Game.s("Happy Little Dinosaurs")
+default_collection.games << Game.s("Sushi Go!")
+default_collection.games << Game.s("Taco Cat Goat Cheese Pizza")
